4535888acf1e1ac9633550e4292afd0ae75a5b6e
shankpaul <shanpaul07@gmail.com>
2013-11-29 16:49:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var io = require('socket.io');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3002);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
